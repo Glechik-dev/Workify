7e9d7e578ea9eeebaf5779f23eb280c0132dcb60
Glechik <eg2akkfall@gmail.com>
2025-09-06 21:53:28 +0300

    feat: added AuthController

---
+++ src/Workify.Api/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Workify.Api.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [Route("login")]
+        [HttpGet]
+        public IActionResult login() 
+        {
+            return Ok();
+        }
+
+        [Route("sign-up")]
+        [HttpGet]
+        public IActionResult signUp()
+        {
+            return Ok();
+        }
+
+        [Route("log-out")]
+        [HttpGet]
+        public IActionResult logOut()
+        {
+            return Ok();
+        }
+    }
+}
